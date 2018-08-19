c795015a448e53408ea6677d9776bb9e20289969
Jose Vinicius <jviniciusoliveira@gmail.com>
2018-08-19 17:39:55 -0300

    Melhorando estrutura do projeto

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ app.listen(3002, function() {
     app.get('/', function(req, res) {
         res.render('index');
     });
+
+    app.get('/votar', function(req, res) {
+        res.render('votar');
+    });
 });
