6802370cdc77468a8e719b6daa390d9cf60560ad
Jose Vinicius <jviniciusoliveira@gmail.com>
2018-08-20 12:00:32 -0300

    Habilitando auto-load com Consign

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 var app = require('./app/config/AppExpress')();
 
 app.listen(3002, function() {
-    
-    app.get('/', function(req, res) {
-        res.render('index');
-    });
-
-    app.get('/votar', function(req, res) {
-        res.render('votar');
-    });
+    console.log('Server Up.');
 });
--- app/config/AppExpress.js
+++ app/config/AppExpress.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var consign = require('consign');
 
 
 module.exports = function() {
+    
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+
+    consign({cwd: 'app'})
+        .include('routes')
+        .into(app);
 
     return app;
 }
