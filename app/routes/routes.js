6d9fd7f430d91545949bf4c85f95fac6048989f5
Jose Vinicius <jviniciusoliveira@gmail.com>
2018-08-24 11:54:03 -0300

    Modal autenticação

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,4 +1,11 @@
 module.exports = function(app) {
 
-
+    app.post('/login', function(req, res) {
+        console.log(req.route.path);
+        if(req.body.user == 'admin' && req.body.pass == 123) {
+            res.redirect('/gerenciar');
+        } else {
+            res.render('');
+        }        
+    });
 }
